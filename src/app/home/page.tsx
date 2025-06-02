f2b78e3cfc4b427abac56078553a09061a6ebd49
uspilledtea <nguyenhbryan0818@gmail.com>
2025-06-02 00:00:23 -0400

    firebase auth, home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <h1 className="text-4xl font-bold mb-4 text-white">
+        Welcome to the Home Page
+      </h1>
+      <p className="text-lg text-gray-700">This is a simple home page.</p>
+    </div>
+  );
+}
